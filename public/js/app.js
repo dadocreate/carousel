b2eced98778a70db5e3372b737cfbd29f65e78a6
Davide Marco Fortuna <davidefortuna@gmail.com>
2015-11-18 17:03:33 +0000

    css customisation and fine tuning bullets selection

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,9 @@ $(function(){
     'use strict';
 
     // debug
-    var script = document.createElement('script');
-    script.setAttribute('src', 'http://localhost:35729/livereload.js');
-    document.body.appendChild(script);
+    // var script = document.createElement('script');
+    // script.setAttribute('src', 'http://localhost:35729/livereload.js');
+    // document.body.appendChild(script);
 
     function retriveContents(){
         $.ajax({
@@ -14,20 +14,18 @@ $(function(){
             success: function(data) {
                 //data downloaded so we call parseJSON function
                 //and pass downloaded data
-
                 var json = $.parseJSON(data);
                 console.log(JSON.stringify(json));
 
                 //now json variable contains data in json format
                 //let's display a few items
                 $.each(json.data.carousel.items, function(i, e) {
-                    $('.items').append('<li class="item"><a href="'+ e.url +'"><img src="'+ e.img +'"alt="'+ e.alt +'"/></a><a href="'+ e.url +'"><h3>'+ e.title +'</h3></a><p>'+ e.content +'</p></li>')
+                    $('.items').append('<li class="item"><a href="'+ e.url +'"><img src="'+ e.img +'"alt="'+ e.alt +'"/></a><a href="'+ e.url +'"><h3 class="title">'+ e.title +'</h3></a><p>'+ e.content +'</p></li>')
                 });
 
                 initCarousel();
             }
         });
-
     }
 
     function initCarousel(){
@@ -39,24 +37,31 @@ $(function(){
         var slidesBlockWidth = slideWidth * 4;
         var direction;
 
+        // Create a div containrer
         slides.wrapAll('<div id="slidesHolder"></div>');
         slides.css({ 'float' : 'left' });
+        // Calculate div width
         $('#slidesHolder').css('width', slideWidth * numberOfSlides);
 
         // create nav buttons
         for (var i = 0; i < numberOfSlidesBlock; i++) {
-            $('.buttons').append('<li><a  class="navButtons" href="#" id="'+ i +'">'+ i +'</a></li>');
+                $('.buttons').append('<li><a  class="navButtons" href="#" id="'+ i +'"></a></li>');
         }
 
+        // Pre-select starting block bullet
+        selectedBullet();
+
+        // Arrow events handler
         $('.navButtons').bind('click', function() {
+            // Remove all selections
             $('.navButtons').removeClass('selected');
             $(this).addClass('selected');
-
-            console.log($(this).attr('id'));
+            // Add current selection
             currentPosition = $(this).attr('id');
             moveSlide();
         });
 
+        // managing blocks rotation
         function changePosition() {
             if(direction == "right"){
                 if (currentPosition >= numberOfSlidesBlock - 1) {
@@ -74,15 +79,26 @@ $(function(){
         	moveSlide();
         }
 
+        // Animate blocks
         function moveSlide() {
         	$('#slidesHolder').animate({'marginLeft' : slidesBlockWidth*(-currentPosition)});
+            selectedBullet();
         }
 
-        //tell the buttons what to do when clicked
+        // Bullets selection
+        function selectedBullet() {
+            // Remove all selections
+            $('.navButtons').removeClass('selected');
+            // Add current selection
+            var currentBullet = $('#'+currentPosition);
+            $(currentBullet).addClass('selected');
+        }
+
+        // Tell the buttons what to do when clicked
 		$('.nav').bind('click', function() {
             console.log($(this).attr('id'));
-			//determine new position
-            if(($(this).attr('id')=='#rightNav')){
+			//determine new direction
+            if(($(this).attr('id')=='rightNav')){
                 direction = "right";
             }else{
                 direction = "left"
@@ -90,8 +106,5 @@ $(function(){
             changePosition();
 		});
     }
-
     retriveContents();
-
-
 });
